69faf059dc18d192d167ec92b2c847f216896bf2
Steven Lee <stevenlee324@gmail.com>
2020-09-06 02:09:36 -0700

    Add core app

---
+++ app/core/tests/test_create_user.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_password(self):
+        """Test creating user with email and password"""
+
+        email = "someone@company.com"
+        password = "secretpassword"
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
