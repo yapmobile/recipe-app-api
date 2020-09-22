00a931e51ef97c81fc3125703e44c970bd6b9712
Steven Lee <stevenlee324@gmail.com>
2020-09-22 02:10:13 -0700

    Add receipt app.  Added Tag and ingredient

--- app/core/tests/test_create_user.py
+++ app/core/tests/test_create_user.py
@@ -1,14 +1,21 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='sampleuser@sample.com', password='samplepassword'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_password(self):
         """Test creating user with email and password"""
 
-        email = "someone@company.com"
-        password = "secretpassword"
+        email = 'someone@company.com'
+        password = 'secretpassword'
 
         user = get_user_model().objects.create_user(
             email=email,
@@ -20,9 +27,9 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalize(self):
         """Test the email of a new user is normailized"""
-        email = "someone@COMPANY.COM"
+        email = 'someone@COMPANY.COM'
 
-        user = get_user_model().objects.create_user(email=email, password="abc")
+        user = get_user_model().objects.create_user(email=email, password='abc')
 
         self.assertEqual(user.email, email.lower())
 
@@ -36,9 +43,27 @@ class ModelTests(TestCase):
         """Test creating new super user"""
 
         superuser = get_user_model().objects.create_superuser(
-            "superuer@company.com",
-            "supersecret"
+            'superuer@company.com',
+            'supersecret'
         )
 
         self.assertTrue(superuser.is_staff)
         self.assertTrue(superuser.is_superuser)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='sampleTagName'
+        )
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
---
+++ app/recipe/tests/test_ingredient_api.py
@@ -0,0 +1,87 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+
+INGREDIENT_URL = reverse('recipe:ingredient-list')
+
+
+class PublicTagsApiTests(TestCase):
+    """Test the public available test api"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+
+class PrivateTagsApiTests(TestCase):
+    """Test the authorized user tags API"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'user1@company.com',
+            'secretPassword',
+        )
+
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_login_required(self):
+        """Test that login is required for retrieving tags"""
+        self.client.force_authenticate(user=None, token=None)
+
+        res = self.client.get(INGREDIENT_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_retrieve_ingredient_list(self):
+        """Test retrieving ingredients"""
+        Ingredient.objects.create(user=self.user, name='Kale')
+        Ingredient.objects.create(user=self.user, name='Salt')
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        res = self.client.get(INGREDIENT_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        user2 = get_user_model().objects.create_user(
+            'user2@company.com',
+            'secretpassword2',
+        )
+
+        Ingredient.objects.create(user=user2, name='Vinegar')
+
+        ingredient = Ingredient.objects.create(user=self.user, name='Tumeric')
+
+        res = self.client.get(INGREDIENT_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    # def test_create_tag_successful(self):
+    #     """Test creating a new tag"""
+    #     payload = {'name': 'Test tag'}
+    #     self.client.post(TAGS_URL, payload)
+    #
+    #     exists = Tag.objects.filter(
+    #         user=self.user,
+    #         name=payload['name']
+    #     ).exists()
+    #     self.assertTrue(exists)
+    #
+    # def test_create_tag_invalid(self):
+    #     """Test creating a new tag with invalid payload"""
+    #     payload = {'name': ''}
+    #     res = self.client.post(INGREDIENT_URL, payload)
+    #
+    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
