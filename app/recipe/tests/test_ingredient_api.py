eedf1ed5cd6f571f2d128a9b5f555a72aed5529d
Steven Lee <stevenlee324@gmail.com>
2020-09-23 01:29:08 -0700

    Add ingredient create endpoint and test

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -68,20 +68,20 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
 
-    # def test_create_tag_successful(self):
-    #     """Test creating a new tag"""
-    #     payload = {'name': 'Test tag'}
-    #     self.client.post(TAGS_URL, payload)
-    #
-    #     exists = Tag.objects.filter(
-    #         user=self.user,
-    #         name=payload['name']
-    #     ).exists()
-    #     self.assertTrue(exists)
-    #
-    # def test_create_tag_invalid(self):
-    #     """Test creating a new tag with invalid payload"""
-    #     payload = {'name': ''}
-    #     res = self.client.post(INGREDIENT_URL, payload)
-    #
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_create_ingredient_successful(self):
+        """Test creating a new ingredient"""
+        payload = {'name': 'Vinegar'}
+        self.client.post(INGREDIENT_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating a new ingredient with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,6 +38,6 @@ class IngredientViewSet(viewsets.GenericViewSet,
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    # def perform_create(self, serializer):
-    #     """Create a new tag"""
-    #     serializer.save(user=self.request.user)
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
